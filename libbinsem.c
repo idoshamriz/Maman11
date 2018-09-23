6b57a02a5f8b9a61dd5d18735ee8c0d7041aa13d
Ido Shamriz <ido.shamriz@gmail.com>
2018-09-23 06:39:16 -0700

    Finished the libbinsem

--- libbinsem.c
+++ libbinsem.c
@@ -1,25 +1,50 @@
 #include "binsem.h"
 
-static
+#define LOCKED 0
+#define UNLOCKED 1
+#define PID_ERROR -1
 
 
 void binsem_init(sem_t *s, int init_val) {
-    if (init_val == 0)
-        xchg(s, 0);
-    else
-        xchg(s, 1);
+    if (s == NULL) {
+        fprintf(stderr, "Error: invalid value for semaphore init");
+        exit(SYS_ERR);
+    }
+
+    // Setting the semaphore's initial value based on the received input
+    sem_t sem_init_value = (LOCKED == init_val) ? LOCKED : UNLOCKED;
+    xchg(s, sem_init_value)
 }
 
 void binsem_up(sem_t *s) {
-
+    // Informing that the critical section can be entered
+    xchg(s, UNLOCKED);
 }
 
 int binsem_down(sem_t *s) {
+    pid_t current_process_id = NULL;
+    sem_t use_semphore = LOCKED;
 
-    if (*s == 0) {
-        // Blocking by exersice's definition
-        alarm(0);
+    // "Blocking" the process until the semaphore is released
+    while (LOCKED == use_semphore) {
+
+        // Trying to lock the semaphore to the current thread
+        use_semphore = xchg(s, LOCKED);
+
+        // If another thread already locked the semaphor, "block" the current one (by sending an alarm and
+        // run the following thread
+        if (LOCKED == lock_semphore) {
+            current_process_id = getpid();
+
+            if (PID_ERROR == kill(current_process_id, SIGALRM)) {
+                fprintf(stderr, "Failed to block current thread");
+                return SYS_ERR;
+            }
+        }
     }
-    else if (*s > 0)
-        xchg(s, s - 1);
+
+    // Exiting the while loop states that the current thread managed to LOCK an UNLOCKED semaphore
+    // atomically, which means it is currently running
+
+    return 0;
 }
